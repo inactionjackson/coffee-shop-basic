3554f4d30381882dbb2087480863980f0becf57e
Andrew Jackson <inactionjackson@gmail.com>
2019-09-28 02:16:47 -0400

    added menu functionality

--- scripts.js
+++ scripts.js
@@ -1,10 +1,7 @@
 (() => {
-  window.onbeforeunload = () => {
-    document.querySelector("nav").classList.remove("nav-scrolled");
-    window.scrollTo(0, 0);
-  };
+  const hostURL = "localhost:5500";
   //#region (nav scrolling)
-  let bCheckScroll = false;
+  let bCheckScroll = true;
 
   const pageStartRect = document
     .querySelector("#page-start")
@@ -32,6 +29,24 @@
       }
     }
   }
+  const navScrollHandler = (e) => {
+    if (bCheckScroll) {
+      bCheckScroll = false;
+      if (window.scrollY >= navHeight) {
+        changeNavClasses(true, true);
+      } else if (window.scrollY < navHeight * 0.5 && document.querySelector("nav").classList.contains("nav-scrolled")) {
+        changeNavClasses(false, true);
+      }
+    } else {
+      setTimeout(() => {
+        bCheckScroll = true;
+      }, 30);
+    }
+  }
+  if (window.location.hash) {
+    changeNavClasses(true, false);
+  }
+  window.addEventListener("scroll", navScrollHandler);
   /** force scroll to location, causing jittering with css smooth scroll */
   // const scrollDownToPageStart = animated => {
   //   changeNavClasses(true, animated);
@@ -49,27 +64,54 @@
   //   window.scroll({ top: 0, left: 0, behavior: ScrollBehavior });
   // };
 
-
-  window.addEventListener("scroll", e => {
-    console.log(window.scrollY);
-    if (bCheckScroll) {
-      bCheckScroll = false;
-      if (
-        window.scrollY >= navHeight &&
-        !document.querySelector("nav").classList.contains("nav-scrolled")
-      ) {
-        changeNavClasses(true, true);
-      } else if (
-        window.scrollY < navHeight * 0.5 &&
-        document.querySelector("nav").classList.contains("nav-scrolled")
-      ) {
-        changeNavClasses(false, true);
-      }
-    } else {
-      setTimeout(() => {
-        bCheckScroll = true;
-      }, 30);
-    }
-  });
   //#endregion (nav scrolling)
+
+  const placeholderDesc = `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi sint
+ alias culpa quibusdam deleniti incidunt soluta tenetur, beatae voluptates nam ipsam
+ possimus quod sed nobis cupiditate, facere est laborum perspiciatis. Vel deleniti fuga
+ saepe iure, enim voluptatum quisquam molestias reprehenderit repellendus. Dolorem,
+ aliquam provident nulla voluptas odit repellendus dolore fuga!`;
+  const getRandPrice = (min, max) => {
+    return Math.max(Math.random() * max, min);
+  }
+  const placeholderFoodMenu = new Array(20).fill({ name: "Food item", price: "$" + getRandPrice(.99, 12).toFixed(2), desc: placeholderDesc });
+  const placeholderDrinkMenu = new Array(20).fill({ name: "Drink item", price: "$" + Math.max(Math.random() * 50, 3.99).toFixed(2), desc: placeholderDesc });
+
+  //TODO: replace menus with actual list from ajax call
+  const foodMenu = placeholderFoodMenu;
+  const drinkMenu = placeholderDrinkMenu;
+  const foodMenuPictureSrc = '/imgs/placeholders/nathan-dumlao-vbt-Fp3b5FA-unsplash.jpg';
+  const drinkMenuPictureSrc = '/imgs/placeholders/joseph-gonzalez-fdlZBWIP0aM-unsplash.jpg';
+
+  const toggleMenuHandler = (e, manualMenu) => {
+    let target = "";
+    if (e) {
+      target = e.target.getAttribute("data-target");
+    } else if (manualMenu) {
+      target = manualMenu;
+    } else {
+      return;
+    }
+    const fillMenu = (items) => {
+      return ('<ul>' + items.reduce((menu, item) => {
+        const listing = '<li><span class="item-name">' + item.name + '</span><span class="item-price">' + item.price + '</span><p class="item-desc">' + item.desc + '</p></li>'
+        return menu + listing;
+      }, "") + '</ul>')
+    }
+    switch (target) {
+      case "menu-drink":
+        document.querySelector("#menu-content").innerHTML = fillMenu(drinkMenu) + '<img src="http://' + hostURL + drinkMenuPictureSrc + '" />';
+        break;
+      case "menu-food":
+        document.querySelector("#menu-content").innerHTML = fillMenu(foodMenu) + '<img src="http://' + hostURL + foodMenuPictureSrc + '" />';
+        break;
+      default:
+        break;
+    }
+  }
+
+  toggleMenuHandler(null, "menu-drink"); //TODO: make this happen after promise getting menu items
+  document.querySelectorAll(".menu-toggle").forEach(el => {
+    el.addEventListener("click", toggleMenuHandler);
+  });
 })();
